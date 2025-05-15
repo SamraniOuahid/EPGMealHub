78b46e64a5d82355b5ad46403a12e6de94d5dc31
ouahidsamrani <ouahid.samrani@usmba.ac.ma>
2025-05-15 16:37:16 +0100

    ff

---
+++ front-end/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+
+export default function Footer() {
+    return (
+        <footer>
+        <div className="footer">
+            <p>© 2023 Your Company. All rights reserved.</p>
+            <p>Follow us on:</p>
+            <ul>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Instagram</a></li>
+            </ul>
+        </div>
+        </footer>
+    );
+    }
