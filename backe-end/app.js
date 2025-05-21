015015b8ed2e88866a5339fe59926b8913e28186
ouahidsamrani <ouahid.samrani@usmba.ac.ma>
2025-05-21 19:56:28 +0100

    dt

--- backe-end/app.js
+++ backe-end/app.js
@@ -4,20 +4,16 @@ const connectDB = require("./config/database");
 const mealRoutes = require("./routes/mealRoutes");
 const userRoutes = require("./routes/userRoutes");
 const commandeRoutes = require("./routes/commandeRoutes");
-const app = express();
+const cors = require("cors");
+const path = require("path");
 
+const app = express();
 
 // Connexion à MongoDB
 connectDB();
 
-
-
-
-
-
-
-
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
@@ -25,6 +21,8 @@ app.use("/api", mealRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/commandes", commandeRoutes);
 
+// ➕ Servir les fichiers images
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Démarrer le serveur
 const PORT = process.env.PORT || 5000;
